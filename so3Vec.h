bdac1d33ae51b560d2d8467959036d3fc147100b
Raghav Navaratna <rsnavaratna@gmail.com>
2023-02-21 13:09:36 -0600

    hat operator on so(3)

---
+++ so3Vec.h
@@ -0,0 +1,15 @@
+#include <eigen3/Eigen/Core>
+
+Eigen::Matrix3d so3_vec(const Eigen::Vector3d& X) {
+  Eigen::Matrix3d g;
+  if (X.size() == 3) { // If input is skew-sym change to vector
+    g << 0, -X(2), X(1),
+         X(2), 0, -X(0),
+         -X(1), X(0), 0;
+  } else { // If input is vector change to skew-sym
+    g << 0, -X(2), X(1),
+         X(2), 0, -X(0),
+         -X(1), X(0), 0;
+  }
+  return g;
+}
