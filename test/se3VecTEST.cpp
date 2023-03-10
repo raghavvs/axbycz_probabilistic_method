b63a57db398689151e468071133daa3f6d4e1f7c
Raghav Navaratna <rsnavaratna@gmail.com>
2023-03-09 20:53:51 -0600

    building and testing utils

--- test/se3VecTEST.cpp
+++ test/se3VecTEST.cpp
@@ -2,37 +2,20 @@
 #include <eigen3/Eigen/Core>
 #include "se3Vec.h"
 
-TEST(Se3VecTest, Handles4x4Matrix) {
-    Eigen::Matrix4d X;
-    X << 1, 0, 0, 2,
-         0, 1, 0, 3,
-         0, 0, 1, 4,
-         0, 0, 0, 1;
+TEST(se3VecTest, MatrixConversion) {
+    Eigen::Matrix4d inputMatrix;
+    inputMatrix << 1, 0, 0, 1,
+                   0, 1, 0, 2,
+                   0, 0, 1, 3,
+                   0, 0, 0, 1;
 
-    Eigen::Matrix<double, 6, 1> expected;
-    expected << 0, 0, 0, 2, 3, 4;
+    Eigen::Matrix<double, 6, 1> expectedOutput;
+    expectedOutput << 0, 0, 0, 1, 2, 3;
 
-    Eigen::Matrix<double, 6, 1> result = se3Vec(X);
+    Eigen::Matrix<double, 6, 1> actualOutput = se3Vec(inputMatrix);
 
-    ASSERT_TRUE(result.isApprox(expected));
+    ASSERT_EQ(actualOutput, expectedOutput);
 }
-
-TEST(Se3VecTest, Handles4x3Matrix) {
-    Eigen::Matrix<double, 4, 3> X;
-    X << 1, 0, 0,
-         0, 1, 0,
-         0, 0, 1,
-         2, 3, 4;
-
-    Eigen::Matrix<double, 6, 1> expected;
-    expected << 0, -4, 3, 2, 4, -1;
-
-    Eigen::Matrix<double, 6, 1> result = se3Vec(X);
-
-    ASSERT_TRUE(result.isApprox(expected));
-}
-
-
 
 int main(int argc, char **argv)
 {
