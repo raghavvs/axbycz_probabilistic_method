716370144f9df359256c611fd26cf9f542c2e52b
Raghav Navaratna <rsnavaratna@gmail.com>
2023-03-09 23:35:41 -0600

    building and testing utils

--- test/scrambleDataTEST.cpp
+++ test/scrambleDataTEST.cpp
@@ -1,36 +1,50 @@
 #include <gtest/gtest.h>
+#include <iostream>
 #include <eigen3/Eigen/Dense>
-#include "scrambleData.h"
+#include <random>
+#include <algorithm>
+#include <chrono>
+#include "scrambleData.h" 
 
-TEST(ScrambleDataTest, ReturnsSameMatrixWhenScrambleRateIsZero) {
-    // Create input matrix
+TEST(ScrambleDataTest, OutputMatrixHasSameShape) {
     Eigen::MatrixXd M(3, 3);
-    M << 1, 2, 3,
-         4, 5, 6,
-         7, 8, 9;
-
-    // Call function with zero scramble rate
-    Eigen::MatrixXd M_scrambled = scrambleData(M, 0.0);
-
-    // Check that the output is the same as the input
-    ASSERT_TRUE(M_scrambled.isApprox(M));
+    M << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+    double s_rate = 1.0;
+    Eigen::MatrixXd M_perm = scrambleData(M, s_rate);
+    ASSERT_EQ(M_perm.rows(), M.rows());
+    ASSERT_EQ(M_perm.cols(), M.cols());
 }
 
-TEST(ScrambleDataTest, ReturnsDifferentMatrixWhenScrambleRateIsNonZero) {
-    // Create input matrix
-    Eigen::MatrixXd M(3, 3);
-    M << 1, 2, 3,
-         4, 5, 6,
-         7, 8, 9;
 
-    // Call function with non-zero scramble rate
-    Eigen::MatrixXd M_scrambled = scrambleData(M, 0.5);
-
-    // Check that the output is different from the input
-    ASSERT_FALSE(M_scrambled.isApprox(M));
+TEST(ScrambleDataTest, OutputMatrixIsScrambled) {
+    // Test that the output matrix is actually scrambled
+    Eigen::MatrixXd inputMatrix(3, 3);
+    inputMatrix << 1, 2, 3,
+                   4, 5, 6,
+                   7, 8, 9;
+    Eigen::MatrixXd outputMatrix = scrambleData(inputMatrix, 0.5);
+    bool isSame = true;
+    for (int i = 0; i < inputMatrix.cols(); i++) {
+        if (inputMatrix.col(i) != outputMatrix.col(i)) {
+            isSame = false;
+            break;
+        }
+    }
+    EXPECT_FALSE(isSame);
 }
 
+TEST(ScrambleDataTest, OutputMatrixWithZeroScrambleRate) {
+    // Test that the output matrix is the same as the input matrix if scramble rate is 0
+    Eigen::MatrixXd inputMatrix(3, 3);
+    inputMatrix << 1, 2, 3,
+                   4, 5, 6,
+                   7, 8, 9;
+    Eigen::MatrixXd outputMatrix = scrambleData(inputMatrix, 0.0);
+    EXPECT_EQ(inputMatrix, outputMatrix);
+}
+
+// Run the test case
 int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
---
+++ util/permFixABC.h
@@ -0,0 +1,42 @@
+/*
+Generate partially permuted data triples [A1_perm, B1_perm, C1_perm]
+ Input:
+       M in 4 x 4 x 1
+       N in 4 x 4 x n
+       P in 4 x 4 x n
+       r : scrambling rate
+ Output:
+       M_perm in 4 x 4 x n (n copies of M)
+       N_perm in 4 x 4 x n (permuated N)
+       P_perm in 4 x 4 x n (samme as P)
+*/
+
+/*
+DESCRIPTION:
+
+This program defines a function called permFixABC that takes as input three matrices 
+M, N, and P, and a scalar r. It generates partially permuted data triples [A1_perm, B1_perm, C1_perm]. 
+M is a 4x4x1 matrix, N is a 4x4xn matrix, P is a 4x4xn matrix, and r is a scrambling rate. 
+The output consists of three matrices, M_perm which is a copy of M, N_perm which is a 
+permuted version of N, and P_perm which is the same as P. The permuted version of N is 
+obtained by calling the function scrambleData with N and r. The function permFixABC uses 
+Eigen matrices and blocks to resize and fill the matrices M_perm.
+*/
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <scrambleData.h>
+
+void permFixABC(const Eigen::MatrixXd& M, const Eigen::MatrixXd& N, const Eigen::MatrixXd& P, 
+                double r, Eigen::MatrixXd& M_perm, Eigen::MatrixXd& N_perm, Eigen::MatrixXd& P_perm) {
+
+    int n = N.cols();
+    M_perm.resize(M.rows(), M.cols() * n);
+    for (int i = 0; i < n; i++) {
+        M_perm.block(0, i * M.cols(), M.rows(), M.cols()) = M;
+    }
+    N_perm = scrambleData(N, r);
+    P_perm = P;
+}
