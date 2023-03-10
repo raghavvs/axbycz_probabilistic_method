3ce0c772f02d07ebf0d28d02ced2cbc256c3778b
Raghav Navaratna <rsnavaratna@gmail.com>
2023-03-09 22:24:37 -0600

    building and testing utils

--- test/se3VecTEST.cpp
+++ test/se3VecTEST.cpp
@@ -1,24 +1,76 @@
 #include <gtest/gtest.h>
-#include <eigen3/Eigen/Core>
-#include "se3Vec.h"
+#include <eigen3/Eigen/Dense>
+#include "se3Vec.h" // header file containing the se3Vec function
 
-TEST(se3VecTest, MatrixConversion) {
-    Eigen::Matrix4d inputMatrix;
-    inputMatrix << 1, 0, 0, 1,
-                   0, 1, 0, 2,
-                   0, 0, 1, 3,
-                   0, 0, 0, 1;
+// Test fixture class for se3Vec function tests
+class Se3VecTest : public testing::Test {
+protected:
+  Eigen::Matrix<double, 4, 4> X1;
+  Eigen::Matrix<double, 3, 3> X2;
+  Eigen::Matrix<double, 4, 6> X3;
 
-    Eigen::Matrix<double, 6, 1> expectedOutput;
-    expectedOutput << 0, 0, 0, 1, 2, 3;
+  Se3VecTest() {
+    X1 << 1, 2, 3, 4,
+          5, 6, 7, 8,
+          9, 10, 11, 12,
+          13, 14, 15, 16;
 
-    Eigen::Matrix<double, 6, 1> actualOutput = se3Vec(inputMatrix);
+    X2 << 1, 2, 3,
+          4, 5, 6,
+          7, 8, 9;
 
-    ASSERT_EQ(actualOutput, expectedOutput);
+    X3 << 1, 2, 3, 4, 5, 6,
+          7, 8, 9, 10, 11, 12,
+          13, 14, 15, 16, 17, 18,
+          19, 20, 21, 22, 23, 24;
+  }
+};
+
+TEST_F(Se3VecTest, Returns6x1MatrixWhenInputIs4x4Matrix) {
+  Eigen::Matrix<double, 6, 1> expected_g;
+  expected_g << -X1(1, 2), X1(0, 2), -X1(0, 1), X1(0, 3), X1(1, 3), X1(2, 3);
+
+  Eigen::Matrix<double, 6, 1> actual_g = se3Vec(X1);
+
+  ASSERT_EQ(expected_g.rows(), actual_g.rows());
+  ASSERT_EQ(expected_g.cols(), actual_g.cols());
 }
 
-int main(int argc, char **argv)
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+TEST_F(Se3VecTest, Returns4x4MatrixWhenInputIsNot4x4Matrix) {
+  Eigen::Matrix<double, 4, 4> expected_g;
+  expected_g << 0, -X2(2, 1), X2(1, 0), 0,
+                X2(2, 1), 0, -X2(0, 2), 0,
+                -X2(1, 0), X2(0, 2), 0, 0,
+                0, 0, 0, 0;
+
+  Eigen::Matrix<double, 4, 4> actual_g = se3Vec(X2);
+
+  ASSERT_EQ(expected_g.rows(), actual_g.rows());
+  ASSERT_EQ(expected_g.cols(), actual_g.cols());
+}
+
+TEST_F(Se3VecTest, ReturnsCorrectDimensionsForAnotherNon4x4Matrix) {
+    
+    Eigen::Matrix<double, 4, 4> expected_g;
+    expected_g << 0, -X3(2, 1), X3(1, 0), X3(0, 3),
+                X3(2, 1), 0,-X3(0, 1), X3(1, 3),
+                -X3(1, 0), X3(0, 1), 0, X3(2, 3),
+                0, 0, 0, 1;
+
+    // Define the twist vector.
+    Eigen::Matrix<double, 6, 1> twist_vec;
+    twist_vec << 0.1, 0.2, 0.3, 0.4, 0.5, 0.6;
+
+    // Compute the expected result.
+    Eigen::Matrix<double, 4, 4> expected_g_twist = expected_g * se3Vec(twist_vec).exp();
+
+    // Compute the actual result.
+    Eigen::Matrix<double, 4, 4> actual_g_twist = g(twist_vec);
+
+    // Verify the result.
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            ASSERT_NEAR(expected_g_twist(i, j), actual_g_twist(i, j), 1e-9);
+         }
+     }
 }
--- util/se3Vec.h
+++ util/se3Vec.h
@@ -12,18 +12,14 @@ The output vector or matrix is represented by an Eigen object of type
 "Eigen::Matrix<double, 6, 1>".
 */
 
-#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
 
-Eigen::Matrix<double, 6, 1> se3Vec(const Eigen::Matrix4d& X)
-{
-    if (X.cols() == 4) // If input is skew-sym change to vector
-    {
+Eigen::Matrix<double, 6, 1> se3Vec(const Eigen::Matrix<double, 4, 4>& X) {
+    if (X.cols() == 4) {
         Eigen::Matrix<double, 6, 1> g;
-        g << -X(1,2), X(0,2), -X(0,1), X(0,3), X(1,3), X(2,3);
+        g << -X(1, 2), X(0, 2), -X(0, 1), X(0, 3), X(1, 3), X(2, 3);
         return g;
-    }
-    else // If input is vector change to skew-sym
-    {
+    } else {
         Eigen::Matrix<double, 4, 4> g;
         g << 0, -X(2), X(1), X(3),
              X(2), 0, -X(0), X(4),
