5f4191c4ccf93a2933e53ba164d9cd0bdaf90df2
raghavvs <rsnavaratna@gmail.com>
2023-03-17 15:37:41 -0500

    working on the main script to run the real data

---
+++ mainRealData.cpp
@@ -0,0 +1,90 @@
+/*
+DESCRIPTION:
+
+The code is a MATLAB script that tests for iterative refinement on real data for
+ solving the AXB=YCZ calibration problem. The problem is to find three unknown
+ matrices X, Y and Z given three sets of known matrices A, B and C that satisfy
+ the equation AXB=YCZ. The script uses three different methods to solve the
+ problem: Prob 1, Iterative Refinement and Wang Method. Prob 1 is a probabilistic
+ method that uses random sampling and voting to find an approximate solution.
+ Iterative Refinement is a method that improves the solution by minimizing a
+ cost function using linear least squares. Wang Method is a traditional method
+ that uses singular value decomposition and matrix inversion to find an exact solution.
+
+The script loads some real data from files that contain the matrices A, B and
+ C for different poses of a robot head, hand and tag. The script also generates
+ some initial guesses for X, Y and Z based on identity matrices or approximate
+ measurements from kinematics data. The script then scrambles some of the data
+ with different rates to simulate noise or outliers. The script then calls each
+ of the three methods with the original and scrambled data and calculates the
+ error metric for each method. The error metric is defined as the Frobenius norm
+ of the difference between AXB and YCZ for all poses. The script then plots the
+ error versus scramble rate for each method and compares their performance.
+
+The script also defines some supporting functions that convert cell arrays to
+ 3D matrices, calculate the inverse of a 4x4 matrix in SE(3), calculate the adjoint
+ matrix of a 4x4 matrix in SE(3), skew-symmetrize a 3x1 vector, calculate mean and
+ covariance of varying data, scramble data with random permutations, construct M and
+ b matrices for linear least squares problems, and calculate exponential maps of 4x4
+ matrices in SE(3).
+*/
+
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main() {
+    // Add file dependencies
+// TODO: implement a function to load files from different directories
+
+// Load data
+// TODO: implement a function to load data from .mat files
+    vector<vector<Matrix4d>> transform_ABC_unified; // load the experiment data
+    vector<vector<Matrix4d>> transform_ABC_unified_fixA;
+    vector<vector<Matrix4d>> transform_ABC_unified_fixC;
+
+// Generate Data
+// Initial guesses:
+//  1 for identity; 2(or 3) for approximate measurement from kinematics
+//  data of the robot; 3 for results from Prob 1.
+    int init_guess = 1;
+
+    Matrix4d X_init;
+    Matrix4d Y_init;
+    Matrix4d Z_init;
+    // Initial guess as Identity if (init_guess == 1) { X_init =
+    Matrix4d::Identity(); Y_init =
+                                  Matrix4d::Identity(); Z_init =
+                                                                Matrix4d::Identity(); } else { //
+Initial guess as approx. measurement X_init <<
+AngleAxisd(-M_PI/2,
+Vector3d::
+UnitX()) * AngleAxisd(M_PI/2,
+Vector3d::
+UnitY()) * AngleAxisd(1.2*M_PI/180,
+Vector3d::
+UnitX()), Vector3d(58.71,0,63.64)/1000; 0,
+0,
+0,
+1; //
+from kinematics data Y_init <<
+AngleAxisd(M_PI,
+Vector3d::
+UnitZ()) * AngleAxisd(M_PI/4,
+Vector3d::
+UnitZ()), Vector3d(400,0,0)/1000; 0,
+0,
+0,
+1; Z_init <<
+AngleAxisd(M_PI,
+Vector3d::
+UnitZ()), Vector3d(0,0,10)/1000; 0,
+0,
+0,
+1; } return
+EXIT_SUCCESS;
+}
+}
