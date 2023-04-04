f4b0f118bf97f4a4e63b0c00f8855260f4d7531c
Raghav Navaratna <rsnavaratna@gmail.com>
2023-04-04 17:59:32 -0500

    reading tf txt file and converting it to matrices for calibration

--- scripts/readMatrices.cpp
+++ scripts/readMatrices.cpp
@@ -4,10 +4,44 @@
 
 // Example code snippet for reading matrices from a text file
 
+#include <iostream>
 #include <fstream>
-#include <vector>
 #include <Eigen/Dense>
 
+int main() {
+    std::string file_path = "data/000.txt";
+    std::ifstream file(file_path);
+    Eigen::Matrix4d matrix;
+
+    if (file.is_open()) {
+        std::string line;
+        int row = 0;
+
+        while (std::getline(file, line) && row < 4) {
+            std::istringstream iss(line);
+            double value;
+            int col = 0;
+
+            while (iss >> value && col < 4) {
+                matrix(row, col) = value;
+                col++;
+            }
+
+            row++;
+        }
+    } else {
+        std::cerr << "Unable to open the file: " << file_path << std::endl;
+        return 1;
+    }
+
+    // Print the 4x4 matrix
+    std::cout << matrix << std::endl;
+
+    return 0;
+}
+
+
+/*
 int main() {
     std::ifstream file("data.txt");
     std::vector<Eigen::Matrix4d> MatA;
@@ -18,4 +52,4 @@ int main() {
                 file >> mat(i,j);
         MatA.push_back(mat);
     }
-}
+}*/
