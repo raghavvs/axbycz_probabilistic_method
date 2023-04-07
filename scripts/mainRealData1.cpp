d2c2ba27a1bed3a85e152c2f7bb6592964c0dd45
Raghav Navaratna <rsnavaratna@gmail.com>
2023-04-06 19:25:20 -0500

    tf collection (fix - input matrices not invertible)

--- scripts/mainRealData1.cpp
+++ scripts/mainRealData1.cpp
@@ -56,12 +56,12 @@ int main()
 {
     std::vector<Eigen::Matrix4d> A1, B1, C1, A2, B2, C2;
 
-    std::vector<std::string> A1_files = {"data/C_fixed/r1_tf.txt"};
-    std::vector<std::string> B1_files = {"data/C_fixed/c2b_tf.txt"};
-    std::vector<std::string> C1_files = {"data/C_fixed/r2_tf.txt"};
-    std::vector<std::string> A2_files = {"data/A_fixed/r1_tf.txt"};
-    std::vector<std::string> B2_files = {"data/A_fixed/c2b_tf.txt"};
-    std::vector<std::string> C2_files = {"data/A_fixed/r2_tf.txt"};
+    std::vector<std::string> A1_files = {"data/r1_tf1.txt"};
+    std::vector<std::string> B1_files = {"data/c2b_tf1.txt"};
+    std::vector<std::string> C1_files = {"data/r2_tf1.txt"};
+    std::vector<std::string> A2_files = {"data/r1_tf1.txt"};
+    std::vector<std::string> B2_files = {"data/c2b_tf1.txt"};
+    std::vector<std::string> C2_files = {"data/r2_tf1.txt"};
 
     loadMatrices(A1_files, A1);
     loadMatrices(B1_files, B1);
@@ -97,11 +97,11 @@ int main()
 
     double err1, err3;
 
-    /*std::cout << "Probability Method 1..." << std::endl;
+    std::cout << "Probability Method 1..." << std::endl;
     axbyczProb1(A1, B1, C1,
                 A2, B2, C2,
                 0, 0, 0,
-                X_cal1, Y_cal1, Z_cal1);*/
+                X_cal1, Y_cal1, Z_cal1);
 
     // Initial guess for iterative refinement as the results from prob 1
     if (init_guess == 3) {
@@ -112,7 +112,7 @@ int main()
 
     // Iterative Refinement
     std::cout << "Iterative Refinement..." << std::endl;
-    int num = 498;
+    int num = 1;
     axbyczProb3(A1, B1, C1,
                 A2, B2, C2,
                 X_init, Y_init, Z_init,
@@ -121,8 +121,8 @@ int main()
 
     // Verification
     // Prob 1
-    /*err1 = metric(A1, B1, C1, X_cal1, Y_cal1, Z_cal1) +
-                        metric(A2, B2, C2, X_cal1, Y_cal1, Z_cal1);*/
+    err1 = metric(A1, B1, C1, X_cal1, Y_cal1, Z_cal1) +
+                        metric(A2, B2, C2, X_cal1, Y_cal1, Z_cal1);
 
     // Iterative refinement
     err3 = metric(A1, B1, C1, X_cal3, Y_cal3, Z_cal3) +
@@ -131,10 +131,10 @@ int main()
 
     std::ofstream outFile("results/XYZ.txt", std::ios_base::app);
 
-    /*outFile << "Probability Method 1" << std::endl;
+    outFile << "Probability Method 1" << std::endl;
     outFile << "X_cal1: " << std::endl << X_cal1 << std::endl;
     outFile << "Y_cal1: " << std::endl << Y_cal1 << std::endl;
-    outFile << "Z_cal1: " << std::endl << Z_cal1 << std::endl;*/
+    outFile << "Z_cal1: " << std::endl << Z_cal1 << std::endl;
     outFile << "Probability Method 3 - Iterative Refinement" << std::endl;
     outFile << "X_cal3: " << std::endl << X_cal3 << std::endl;
     outFile << "Y_cal3: " << std::endl << Y_cal3 << std::endl;
@@ -142,6 +142,34 @@ int main()
 
     outFile.close();
 
-    //std::cout << "Error 1: " << err1 << std::endl;
+    std::cout << "Error 1: " << err1 << std::endl;
     std::cout << "Error 3: " << err3 << std::endl;
 }
+
+/*
+ * Error:
+ *
+ * Probability Method 1...
+Signal: SIGSEGV (Segmentation fault)
+
+ [mainRealData1] _mm_load_pd emmintrin.h:124
+[mainRealData1] Eigen::internal::pload<double __vector(2)>(Eigen::internal::unpacket_traits<double __vector(2)>::type const*) PacketMath.h:307
+[mainRealData1] Eigen::internal::ploadt<double __vector(2), 16>(Eigen::internal::unpacket_traits<double __vector(2)>::type const*) GenericPacketMath.h:463
+[mainRealData1] Eigen::internal::evaluator<Eigen::PlainObjectBase<Eigen::Matrix<double, 4, 4, 0, 4, 4> > >::packet<16, double __vector(2)>(long, long) const CoreEvaluators.h:197
+[mainRealData1] Eigen::internal::generic_dense_assignment_kernel<Eigen::internal::evaluator<Eigen::Matrix<double, 4, 4, 0, 4, 4> >, Eigen::internal::evaluator<Eigen::Matrix<double, 4, 4, 0, 4, 4> >, Eigen::internal::assign_op<double, double>, 0>::assignPacket<16, 16, double __vector(2)>(long, long) AssignEvaluator.h:652
+[mainRealData1] Eigen::internal::generic_dense_assignment_kernel<Eigen::internal::evaluator<Eigen::Matrix<double, 4, 4, 0, 4, 4> >, Eigen::internal::evaluator<Eigen::Matrix<double, 4, 4, 0, 4, 4> >, Eigen::internal::assign_op<double, double>, 0>::assignPacketByOuterInner<16, 16, double __vector(2)>(long, long) AssignEvaluator.h:666
+[mainRealData1] Eigen::internal::copy_using_evaluator_innervec_CompleteUnrolling::run AssignEvaluator.h:274
+[mainRealData1] Eigen::internal::dense_assignment_loop::run AssignEvaluator.h:468
+[mainRealData1] Eigen::internal::call_dense_assignment_loop<…> AssignEvaluator.h:741
+[mainRealData1] Eigen::internal::Assignment::run AssignEvaluator.h:879
+[mainRealData1] Eigen::internal::call_assignment_no_alias<…> AssignEvaluator.h:836
+[mainRealData1] Eigen::internal::evaluator_assume_aliasing<Eigen::Matrix<double, 4, 4, 0, 4, 4>, Eigen::internal::evaluator_traits<Eigen::Matrix<double, 4, 4, 0, 4, 4> >::Shape>::value, void*>::type) AssignEvaluator.h:804
+[mainRealData1] Eigen::internal::call_assignment<…> AssignEvaluator.h:782
+[mainRealData1] Eigen::PlainObjectBase::_set<…> PlainObjectBase.h:714
+[mainRealData1] Eigen::Matrix::operator= Matrix.h:208
+[mainRealData1] axbyczProb1 axbyczProb1.h:152
+[mainRealData1] main mainRealData1.cpp:101
+[libc.so.6] __libc_start_main 0x00007ffff7513083
+[mainRealData1] _start 0x0000555555557a5e
+
+ */
