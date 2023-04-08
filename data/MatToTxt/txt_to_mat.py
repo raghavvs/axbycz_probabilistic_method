5a274a51bace5ff82c3827462d9800cdcdf2655f
raghavvs <rsnavaratna@gmail.com>
2023-04-08 16:59:44 -0500

    mat to text py script added

---
+++ data/MatToTxt/txt_to_mat.py
@@ -0,0 +1,15 @@
+import numpy as np
+import scipy.io
+
+def txt_to_mat(txt_file, mat_file):
+    # Load the .txt file
+    data = np.loadtxt(txt_file, delimiter=' ')
+
+    # Save the data to a .mat file
+    scipy.io.savemat(mat_file, {'data': data})
+
+# Replace these with the paths to your input .txt file and output .mat file
+input_txt_file = "r1_tf1.txt"
+output_mat_file = "r1_tf1.mat"
+
+txt_to_mat(input_txt_file, output_mat_file)
