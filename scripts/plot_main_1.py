0a100c288ac03c2d6484ff75ed4dbe26a3243d13
Raghav Navaratna <rsnavaratna@gmail.com>
2023-05-02 16:30:27 -0500

    results updated

--- scripts/plot_main_1.py
+++ scripts/plot_main_1.py
@@ -5,10 +5,10 @@ data = pd.read_csv("/home/raghav/cws/axbycz_probabilistic_method/results/results
 
 plt.plot(data["Num_Matrices"].to_numpy(), data["Error1"].to_numpy(), "o-r", label="Probabilistic")
 plt.plot(data["Num_Matrices"].to_numpy(), data["Error3"].to_numpy(), "s-b", label="Iterative")
-plt.xlabel("Number of Datasets")
+plt.xlabel("Number of Data Triplets")
 plt.ylabel("Error")
-plt.title("Error vs Number of Datasets")
+#plt.title("Error vs Number of Data Triplets")
 plt.legend()
 #plt.grid(True)
-plt.savefig("/home/raghav/cws/axbycz_probabilistic_method/results/Error_vs_Datasets_2.png")
+plt.savefig("/home/raghav/cws/axbycz_probabilistic_method/results/Error_vs_Datasets_3.png")
 plt.show()
