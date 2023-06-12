59364f3b7b362dd71ad2f97352a8c2d95a0ed8e4
raghavvs <rsnavaratna@gmail.com>
2023-06-12 12:17:28 +0800

    pose from homography matrix

--- util/ChessBoardPointsImgToTF.cpp
+++ util/ChessBoardPointsImgToTF.cpp
@@ -7,7 +7,7 @@
 
 int main() {
     // Define the number of corners in your chessboard
-    cv::Size patternSize(7, 5);  // for example, a 9x6 chessboard
+    cv::Size patternSize(7, 5);
 
     std::vector<cv::Point2f> corners;
     std::vector<cv::Point3f> objectPoints;
@@ -19,12 +19,13 @@ int main() {
         }
     }
 
-    std::ofstream file("CamToBoardTF.txt");
+    std::ofstream file("data/CamToBoardTF_new.txt");
+    std::ofstream poseFile("data/CamToBoardPose.txt");
 
     // Loop over all images
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
         // Load image
-        cv::Mat img = cv::imread("data/nus_eye_on_hand/" + std::to_string(i) + "+_img_corner.png");
+        cv::Mat img = cv::imread("data/nus_eye_on_hand/" + std::to_string(i) + "_img_corner.png");
 
         // Find chessboard corners
         bool found = cv::findChessboardCorners(img, patternSize, corners);
@@ -34,12 +35,37 @@ int main() {
             cv::Mat H = cv::findHomography(corners, objectPoints);
 
             // Write homography to file
-            file << "Homography for image " << i << ":\n";
             file << H << "\n";
+
+            // Normalization to ensure that ||c1|| = 1
+            double norm = sqrt(H.at<double>(0,0)*H.at<double>(0,0) +
+                               H.at<double>(1,0)*H.at<double>(1,0) +
+                               H.at<double>(2,0)*H.at<double>(2,0));
+            H /= norm;
+            cv::Mat c1  = H.col(0);
+            cv::Mat c2  = H.col(1);
+            cv::Mat c3 = c1.cross(c2);
+            cv::Mat tvec = H.col(2);
+            cv::Mat R(3, 3, CV_64F);
+            for (int i = 0; i < 3; i++)
+            {
+                R.at<double>(i,0) = c1.at<double>(i,0);
+                R.at<double>(i,1) = c2.at<double>(i,0);
+                R.at<double>(i,2) = c3.at<double>(i,0);
+            }
+
+            // Construct the 4x4 pose matrix
+            cv::Mat pose = cv::Mat::eye(4, 4, CV_64F);
+            R.copyTo(pose(cv::Rect(0, 0, 3, 3)));
+            tvec.copyTo(pose(cv::Rect(3, 0, 1, 3)));
+
+            // Write pose matrix to file
+            poseFile << pose << "\n";
         }
     }
 
     file.close();
+    poseFile.close();
 
     return 0;
 }
